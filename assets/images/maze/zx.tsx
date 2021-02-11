7b696c92f667c5cfe1aa38d5145891145bd0666f
Arthur Borges <arthurborges.dev@gmail.com>
2021-02-11 19:49:04 -0300

    Fixed starting point hardcoding values, and deleted assets that aren't being used

--- assets/images/maze/zx.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="zx" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="../li/zx.gif" width="1024" height="1024"/>
-</tileset>
